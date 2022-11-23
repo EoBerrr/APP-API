2c16239f264ef549a3961f3351b18730a1fd5274
GabrielR03 <gabrielrpereira.dev@gmail.com>
2022-11-23 17:00:26 -0300

    fix: tava meio bloatware
    xD

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
-import 'package:path_provider/path_provider.dart';
 import 'dart:async';
 import 'dart:convert';
 
@@ -8,71 +8,121 @@ import 'scr.dart';
 
 void main() {
   runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: FisrtPage(),
-      )
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstPage(),
+    )
   );
 }
 
-class FisrtPage extends StatelessWidget {
-  var _categoryNameController = new TextEditingController();
+class FirstPage extends StatelessWidget {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _categoryNameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar:
-    AppBar(
-      title: const Text('Trabalho Final TP'),
-      centerTitle: true,
-    ),
-      body: Material(
-        color: Colors.white,
-        child: Center(
-          child: ListView(
-            children: <Widget>[
-              const Padding(padding: EdgeInsets.all(30.0)),
-              Image.asset('images/tp.png',
-                width: 200.0,
-                height: 200.0,
-              ),
-              ListTile(
-                title: TextFormField(
-                  controller: _categoryNameController,
-                  decoration:  const InputDecoration(
-                    labelText: 'Search for a category',
-                  ),
-                ),
-              ),
-              const Padding(padding: EdgeInsets.all(10.0)),
-              ListTile(
-                subtitle: Material(
-                  child: MaterialButton(onPressed: (){
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context){
-                        return SecondPage(category: _categoryNameController.text,);
-                      })
-                    )
-                  },),
-                  child: const Text('Search',
-                    style: TextStyle(
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.bold
-                    ),
-                  ),
-                ),
-              ),
-            ]
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('Tecnologia da Programação'),
+        centerTitle: true,
+        backgroundColor: Colors.black87,
+      ),
+      body: Stack(
+        children: <Widget>[
+          const Image(
+            image: AssetImage('images/wallpaper.jpg'),
+            alignment: Alignment.center,
+            height: double.infinity,
+            width: double.infinity,
+            fit: BoxFit.cover,
           ),
-        ),
+          SingleChildScrollView(
+            padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+            child: Form(
+              key: _formKey,
+              child: Center(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30.0),
+                      child: Container(
+                        height: 200.0,
+                        width: 200.0,
+                        decoration: BoxDecoration(
+                          color: const Color.fromRGBO(255, 255, 255, 100),
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        child: Image.asset('images/tp.png'),
+                      )
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.only(top: 30.0),
+                        child: Container(
+                          child: TextFormField(
+                            controller: _categoryNameController,
+                            inputFormatters: [FilteringTextInputFormatter.allow(RegExp("[a-zA-Z]"))],
+                            decoration:  const InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.red),
+                              ),
+                              labelText: "Procure por imagens via categoria",
+                              labelStyle: TextStyle(color: Colors.white),
+                              fillColor: Colors.black54,
+                              filled: true,
+                            ),
+                            style: const TextStyle(color: Colors.white, fontSize: 25.0),
+                            validator: (value){
+                              if(value!.isEmpty){
+                                return "Digite alguma categoria antes de procurar!";
+                              }
+                            },
+                          ),
+                        ),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: Container(
+                          height: 50.0,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.red,
+                              elevation: 15,
+                              shadowColor: Colors.red,
+                            ),
+                            onPressed: (){
+                              if(_formKey.currentState!.validate()){
+                                Navigator.of(context).push(
+                                    MaterialPageRoute(builder: (context){
+                                      return SecondPage(category: _categoryNameController.text);
+                                    })
+                                );
+                              }
+                            },
+                            child: const Text(
+                              'Procurar',
+                              style: TextStyle(
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                          ),
+                        ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
 class SecondPage extends StatefulWidget {
-  const SecondPage({Key? key}) : super(key: key);
   String category;
-  SecondPage({this.category});
+  SecondPage({required this.category});
 
   @override
   State<SecondPage> createState() => _SecondPageState();
@@ -82,44 +132,53 @@ class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white10,
+
       appBar: AppBar(
-        backgroundColor: Colors.white12,
-        title: const Text('Trabalho Final TP',
-          style: TextStyle(color: Colors.deepOrange),
-        ),
+        title: const Text('Tecnologia da Programação'),
         centerTitle: true,
+        backgroundColor: Colors.black87,
       ),
+
       body: FutureBuilder(
         future: _getPics(widget.category),
         builder: (context, snapShot){
           Map? data = snapShot.data;
           if(snapShot.hasError){
-            print(snapShot.error);
-            return const Text('Falha ao conectar com o servidor',
-              style: TextStyle(color: Colors.black),
+            return const Text(
+              'Falha ao conectar com o servidor',
+              style: TextStyle(color: Colors.white, backgroundColor: Colors.black),
             );
           }else if(snapShot.hasData){
             return Center(
               child: ListView.builder(
-                itemCount: data.length,
-                itemBuilder: (context, index),
-                Column(
-                  children: <Widget>[
-                    const Padding(padding: EdgeInsets.all(5.0)),
-                    Container(
-                    child: InkWell(
-                      onTap: (){},
-                      child: Image.network('${data}['hits'][index]['largeImageURL']),
-                    ),
-                    )
-                  ],
-                );
+                itemCount: data!.length,
+                itemBuilder: (context, index){
+                  return Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Container(
+                          width: 300.0,
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(16.0)
+                          ),
+                          child: InkWell(
+                          onTap: (){},
+                          child: Image.network(snapShot.data!["hits"][index]["largeImageURL"]),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
               ),
             );
-          }else if(!snapShot.hasData){
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
+          }else{
+          return const Center(
+          child: CircularProgressIndicator(),
+          );
           }
         }
       ),
@@ -128,7 +187,7 @@ class _SecondPageState extends State<SecondPage> {
 }
 
 Future<Map> _getPics(String category) async {
-  String url = 'https://pixabay.com/api/?key=$apiKey&q=$category&image_type=photo&pretty=true';
-  http.Response response = await http.get(url);
-  return json.decode(response.body);
+String url = 'https://pixabay.com/api/?key=$apiKey&q=$category&image_type=photo&pretty=true';
+http.Response response = await http.get(Uri.parse(url));
+return json.decode(response.body);
 }
